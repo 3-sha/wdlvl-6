6f51e1c74cfa028140877c3782fbebfd3edc5bc6
3-sha <108625495+3-sha@users.noreply.github.com>
2022-12-23 15:19:02 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -13,10 +13,20 @@ module.exports = (sequelize, DataTypes) => {
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
-
-    markAsCompleted() {
-      return this.update({ completed: true });
+    static getTodos() {
+      return this.findAll();
     }
+    static async overdue() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date(),
+          },
+          completed: false,
+        },
+      });
+    }
+   
   }
   Todo.init(
     {
