9f686615b3c4f925253cef52a8775e1092db5231
3-sha <108625495+3-sha@users.noreply.github.com>
2022-12-23 15:27:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require("./app");
-app.listen(3000, () => {
-  console.log("Started express server at port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Started express server at port ${PORT}");
 });
